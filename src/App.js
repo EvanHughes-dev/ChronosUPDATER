23134f51a0b74076e529b84a1e3cee68767834a0
EHughes <ejhingm@comcast.net>
2024-03-04 16:46:57 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import ScheduleSet from './ScheduleSet/ScheduleSet.js'
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import ScheduleSet from "./ScheduleSet/ScheduleSet.js";
+import { useEffect, useState } from "react";
+import "./Header/Header.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const HeaderButtons = ["Default", "Custom"];
 function App() {
+  const [currentSelect, setSelected] = useState("Default");
 
-    const HeadBar = (
-        <div className="header">
-          
-        </div>
-    )
-
+  const Header = () => {
     return (
-        <div>
-            {HeadBar}
-            < ScheduleSet />
-        </div>
+      <div className="topnav">
+        {HeaderButtons.map((buttonName) => {
+          if (buttonName === currentSelect) {
+            return (
+              <a className="active" key={buttonName}>
+                {buttonName}
+              </a>
+            );
+          }
+          return (
+            <a className="inactive" key={buttonName}>
+              <button
+                className="BlankButton"
+                onClick={() => {
+                  setSelected(buttonName);
+                }}
+              >
+                {buttonName}
+              </button>
+            </a>
+          );
+        })}
+      </div>
     );
-
-
+  };
+  return (
+    <div>
+      <Header />
+      <ScheduleSet />
+    </div>
+  );
 }
-
-
 
 export default App;
